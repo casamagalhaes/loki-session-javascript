99c73051acf033dfb895ff61736de42658f935f6
Anderson Andrade <anderson.asa89@gmail.com>
2020-05-08 20:03:35 -0300

    update

--- src/client.ts
+++ src/client.ts
@@ -17,16 +17,41 @@ interface LokiSessionClientOptions {
     appId: string;
     apiVersion?: string;
     endpoint?: string;
+    debug?: boolean;
+}
+
+interface LoggerOptions {
+    debug?: boolean
+}
+
+class Logger {
+    debugMode: boolean
+
+    constructor(options: LoggerOptions) {
+        this.debugMode = options.debug || false
+    }
+
+    debug(message: string, payload?: any) {
+        if (this.debugMode) console.debug(message, payload);
+    }
+
+    error(message: string, payload?: any) {
+        console.error(message, payload);
+    }
 }
 
 export default class LokiSessionClient {
     options: LokiSessionClientOptions;
     websocket: any;
     session: LokiSession;
+    logger: Logger;
 
     constructor(options: LokiSessionClientOptions) {
         this.options = options;
+        this.logger = new Logger({ debug: options.debug || false });
         this.websocket = io(this.endpoint);
+        this.logger.debug(`loki session endpoint: ${this.endpoint}`);
+        this.registeListeners();
     }
 
     get appId() {
@@ -39,8 +64,25 @@ export default class LokiSessionClient {
     }
 
     async registerSession(session: LokiSession) {
+        this.logger.debug(`loki register session:`, session);
         this.session = session;
         this.emit('set_session', session);
+        return this;
+    }
+
+    connected(callback: any) {
+        this.on('connected_stabilished', callback);
+        return this;
+    }
+
+    onSessionInfo(callback: any) {
+        this.on('session_info', callback);
+        return this;
+    }
+
+    onSessionCount(callback: any) {
+        this.on('session_count', callback);
+        return this;
     }
 
     on(event: string, callback: any) {
@@ -49,5 +91,15 @@ export default class LokiSessionClient {
 
     emit(event: string, payload: any) {
         this.websocket.emit(event, payload);
+    }
+
+    private registeListeners() {
+        this.websocket.on('connected_stabilished', () => {
+            this.logger.debug(`loki connected_stabilished:`);
+        });
+
+        this.websocket.on('session_info', (payload: any) => {
+            this.logger.debug(`loki session_info:`, payload);
+        });
     }
 }
